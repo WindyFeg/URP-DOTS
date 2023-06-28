2abefe5631908a4c7b61629509895bc9a454cbdc
WindyFeg <phongspeed7@yahoo.com.vn>
2023-06-28 20:41:06 +0700

    Spawn as formation

--- Assets/Script/Spawn/SpawnSystem.cs
+++ Assets/Script/Spawn/SpawnSystem.cs
@@ -9,125 +9,132 @@ using Unity.Rendering;
 
 public partial struct SpawnSystem : ISystem
 {
+    EntityQuery componentQuery;
+    EntityQuery query;
+    float CurrentLevel;
+
+    [BurstCompile]
+    public partial struct SpawnJob : IJobEntity
+    {
+        // variable
+        public EntityCommandBuffer.ParallelWriter ecb;
+        public float numberOfEnemyInScene;
+        public float level;
+
+        public void Execute(ref LocalTransform tf, ref Spawn spawner)
+        {
+            // action
+            if (spawner.canSpawn && (numberOfEnemyInScene == 0))
+            {
+                ProcessSpawn(ref ecb, spawner);
+            }
+            //Level up
+            // LevelUp(ref ecb, spawner);
+        }
+
+
+        private void ProcessSpawn(ref EntityCommandBuffer.ParallelWriter ecb, Spawn spawner)
+        {
+            // var level = SystemAPI.GetSingleton<GameConfig>()._level;
+            Entity newEnemyEntity = spawner.enemyEntity;
+            // var level = 0;
+            switch (level)
+            {
+                case 0:
+                    for (int i = -5; i <= 5; i++)
+                    {
+                        var pX = i / 2.5f;
+                        float3 position = new float3
+                        {
+                            x = pX,
+                            y = pX * pX,
+                            z = 0
+                        };
+                        ecb.SetComponent(i,
+                            newEnemyEntity,
+                            LocalTransform.FromPosition(position).WithScale(0.5f)
+                            );
+                        ecb.Instantiate(i, newEnemyEntity);
+                    }
+
+                    break;
+
+                case 1:
+                    for (int i = -15; i <= 15; i++)
+                    {
+                        var pX = 16 * Mathf.Sin(i) * Mathf.Sin(i) * Mathf.Sin(i);
+                        var pY = 13 * Mathf.Cos(i) - 5 * Mathf.Cos(2 * i) - 2 * Mathf.Cos(3 * i) - Mathf.Cos(4 * i);
+                        float3 position = new float3
+                        {
+                            x = pX / 5,
+                            y = pY / 5 + 0.5f,
+                            z = 0
+                        };
+                        ecb.SetComponent(i,
+                            newEnemyEntity,
+                            LocalTransform.FromPosition(position).WithScale(0.5f)
+                            );
+                        ecb.Instantiate(i, newEnemyEntity);
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+
+
+
+        private float3 Vector3ToFloat3(Vector3 vector3)
+        {
+            return new float3(vector3.x, vector3.y, vector3.z);
+        }
+    }
+
+    public void OnCreate(ref SystemState state)
+    {
+        componentQuery = new EntityQueryBuilder(Allocator.Temp)
+            .WithAll<LocalTransform>()
+            .WithAll<Spawn>()
+            .Build(ref state);
+
+        query = new EntityQueryBuilder(Allocator.Temp)
+                    .WithAll<Enemy>()
+                    .WithAll<LocalTransform>()
+                    .Build(ref state);
+    }
 
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
+        var ecb = new EntityCommandBuffer(Allocator.TempJob);
+        var numberOfEnemyInScene = query.CalculateEntityCount();
 
-        foreach (RefRW<Spawn> spawner in SystemAPI.Query<RefRW<Spawn>>())
+        if (numberOfEnemyInScene == 0)
         {
-            EntityQuery query = new EntityQueryBuilder(Allocator.Temp)
-                .WithAll<Enemy>()
-                .WithAll<LocalTransform>()
-                .Build(ref state);
-
-            var numberOfEnemyInScene = query.CalculateEntityCount();
-            // Debug.Log(numberOfEnemyInScene);
-            if (spawner.ValueRO.canSpawn && (numberOfEnemyInScene == 0))
-            {
-                // Spawn
-                ProcessSpawn(ref state, spawner);
-                //Level up
-                LevelUp(ref state, spawner, query);
-            }
+            CurrentLevel = LevelUp(ref state);
         }
+        state.Dependency = new SpawnJob
+        {
+            ecb = ecb.AsParallelWriter(),
+            numberOfEnemyInScene = numberOfEnemyInScene,
+            level = CurrentLevel
+        }.ScheduleParallel(state.Dependency);
+
+        state.Dependency.Complete();
+        ecb.Playback(state.EntityManager);
+        ecb.Dispose();
 
     }
 
-    private void LevelUp(ref SystemState state, RefRW<Spawn> spawner, EntityQuery query)
+    private float LevelUp(ref SystemState state)
     {
         // level up
         var gameConfig = SystemAPI.GetSingleton<GameConfig>();
         gameConfig._level += 1;
         SystemAPI.SetSingleton(gameConfig);
+        return gameConfig._level;
     }
 
-    private float3 Vector3ToFloat3(Vector3 vector3)
-    {
-        return new float3(vector3.x, vector3.y, vector3.z);
-    }
 
-    private void ProcessSpawn(ref SystemState state, RefRW<Spawn> spawner)
-    {
-        // Condition Allow a new enemy to spawn
-        // if (spawner.ValueRO.nextSpawnTime < SystemAPI.Time.ElapsedTime)
-        // Check type of spawn
-        var level = SystemAPI.GetSingleton<GameConfig>()._level;
-        Entity newEnemyEntity = spawner.ValueRO.enemyEntity;
-
-        switch (level)
-        {
-            case 0:
-                for (int i = -5; i <= 5; i++)
-                {
-                    var pX = i / 2.5f;
-                    float3 position = new float3
-                    {
-                        x = pX,
-                        y = pX * pX,
-                        z = 0
-                    };
-                    state.EntityManager.SetComponentData(
-                        newEnemyEntity,
-                        LocalTransform.FromPosition(position).WithScale(0.5f)
-                        );
-                    state.EntityManager.Instantiate(newEnemyEntity);
-                }
-
-                break;
-
-            case 1:
-                for (int i = -15; i <= 15; i++)
-                {
-                    var pX = 16 * Mathf.Sin(i) * Mathf.Sin(i) * Mathf.Sin(i);
-                    var pY = 13 * Mathf.Cos(i) - 5 * Mathf.Cos(2 * i) - 2 * Mathf.Cos(3 * i) - Mathf.Cos(4 * i);
-                    float3 position = new float3
-                    {
-                        x = pX / 5,
-                        y = pY / 5 + 0.5f,
-                        z = 0
-                    };
-                    state.EntityManager.SetComponentData(
-                        newEnemyEntity,
-                        LocalTransform.FromPosition(position).WithScale(0.5f)
-                        );
-                    state.EntityManager.Instantiate(newEnemyEntity);
-                }
-                break;
-            case 2:
-                for (int i = -1; i <= 1; i++)
-                {
-                    var pX = i;
-                    var pY = Mathf.Pow(10 - Mathf.Pow(pX, 2 / 3), Mathf.Pow(pX, 3 / 2));
-                    float3 position = new float3
-                    {
-                        x = pX,
-                        y = pY,
-                        z = 0
-                    };
-                    state.EntityManager.SetComponentData(
-                        newEnemyEntity,
-                        LocalTransform.FromPosition(position).WithScale(0.5f)
-                        );
-                    state.EntityManager.Instantiate(newEnemyEntity);
-                }
-                break;
-            default:
-                state.EntityManager.SetComponentData(
-                        newEnemyEntity,
-                        // random position
-                        LocalTransform.FromPosition(
-                            spawner.ValueRO.spawnPosition.x,
-                                UnityEngine.Random.Range(-2f, 5f),
-                                spawner.ValueRO.spawnPosition.z)
-                        );
-
-                break;
-        }
-
-        // reset spawn time
-        // spawner.ValueRW.nextSpawnTime = (float)SystemAPI.Time.ElapsedTime + spawner.ValueRW.spawnRate;
-        // spawner.ValueRW.spawnNumber--;
-
-    }
 }
